2be70b7ab7e62a601736719e91ac707687120e28
NicoMorillo <nicolasm.morillomartinez@gmail.com>
2022-09-29 12:11:41 +0100

    Lambda_function run

---
+++ lambda_function.py
@@ -0,0 +1,15 @@
+import json
+import boto3
+
+dynamodb = boto3.client('dynamodb')
+
+def lambda_handler(event, context):
+    
+    username = event['username']
+    email = event['email']
+    password = event['password']
+    userdetails = event['userdetails']
+    
+    dynamodb.put_item(TableName='nico-lambda-table', Item={'username':{'S':username},'email':{'S':email},'password':{'S':password},'userdetails':{'S':userdetails}})
+    
+    print(email)
